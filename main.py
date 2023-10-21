b18ba147605bcbde6c065b574146703b368a3cce
Saksham Yadav <sakshamyadav11@gmail.com>
2023-10-21 17:52:48 +1100

    changed dataset directory to desktop

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import qe_minesweeper
 
-mine_locations = qe_minesweeper.load_answers("dataset/stage1_training_dataset.h5", 76)
+magnetic_field, dipole = qe_minesweeper.load_dataset("C:\\Users\\saksh\\Desktop\\dataset\\stage1_training_dataset.h5", 1)
 
-print(mine_locations)
+mine_locations = qe_minesweeper.load_answers("C:\\Users\\saksh\\Desktop\\dataset\\stage1_training_dataset.h5", 666)
+mag_east = magnetic_field[0]
+mag_north = magnetic_field[1]
+mag_up = magnetic_field[2]
+
+mine_loc_east = mine_locations[0]
+mine_loc_north = mine_locations[1] 
+mine_1 = (mine_loc_east[0], mine_loc_north[0])
+print(mag_east)
